925ea570a533dd820ebe2da87523313c77beecab
Sophie <sophie16.ou@outlook.com>
2025-07-21 17:35:56 -0400

    final3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Fast-React-Pizza/',
+  base: '/Fast-React-Pizza',
   plugins: [react(), eslint()],
 });
