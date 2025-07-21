02be3b4495b609ea0344007dbf884fe8e2622d6d
Sophie <sophie16.ou@outlook.com>
2025-07-21 15:44:33 -0400

    undo changes

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,11 +4,10 @@ import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
+  console.log(menu);
 
   return (
     <ul className="divide-y divide-stone-300 px-2">
-      <p>change2</p>
-      <p>change3</p>
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
