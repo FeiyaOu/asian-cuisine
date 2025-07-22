79b3b502beb93470f2634ede945a4909401c4184
Sophie <sophie16.ou@outlook.com>
2025-07-22 16:34:22 -0400

    fix1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/asian-cuisine/',
   plugins: [react(), eslint()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        fallback: path.resolve(__dirname, '404.html'), // 👈 Add this!
+      },
+    },
+  },
 });
